e51448225e9d4a812f2f531a4cb7863f6e845cd4
Schmarotzer Sucker T <Schmarotzer Sucker T@DESKTOP-9O0TCOJ.fritz.box>
2016-09-12 13:56:11 +0200

    Local merge completed

---
+++ src/com/tutorial/spellChecker/spellCheckerModule.java
@@ -0,0 +1,18 @@
+package com.tutorial.spellChecker;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class spellCheckerModule {
+	
+	public spellCheckerModule(){
+		System.out.println("SpellcheckerModule init");
+		ApplicationContext contextSpellChecker = new ClassPathXmlApplicationContext("BeanSpellChecker.xml");
+		System.out.println("Bean loaded");
+		
+		ObjectHolder spellCheck = (ObjectHolder) contextSpellChecker.getBean("newSpellcheckinit");
+		System.out.println("Start Spellchecking");
+		spellCheck.getVariable();
+		
+	}
+}
